68af26b6cfed9fbe2d55d8739be0f89aff6db0dc
Guillaume de Monie <guillaumedemonie@gmail.com>
2018-02-25 12:19:44 +0100

    compatibility-for-protect-from-forgery

--- lib/auto_session_timeout.rb
+++ lib/auto_session_timeout.rb
@@ -1,14 +1,13 @@
 module AutoSessionTimeout
-  
+
   def self.included(controller)
     controller.extend ClassMethods
   end
-  
+
   module ClassMethods
-    def auto_session_timeout(seconds=nil)
-      protect_from_forgery except: [:active, :timeout]
+    def auto_session_timeout(seconds=nil, sign_in_path)
       prepend_before_action do |c|
-        if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.now
+        if c.session[:auto_session_expires_at] && c.session[:auto_session_expires_at] < Time.now && (sign_in_path ? !(c.env["PATH_INFO"] == sign_in_path && c.env["REQUEST_METHOD"] == "POST") : true)
           c.send :reset_session
         else
           unless c.request.original_url.start_with?(c.send(:active_url))
@@ -18,23 +17,23 @@ module AutoSessionTimeout
         end
       end
     end
-    
+
     def auto_session_timeout_actions
       define_method(:active) { render_session_status }
       define_method(:timeout) { render_session_timeout }
     end
   end
-  
+
   def render_session_status
     response.headers["Etag"] = ""  # clear etags to prevent caching
     render plain: !!current_user, status: 200
   end
-  
+
   def render_session_timeout
     flash[:notice] = "Your session has timed out."
     redirect_to "/login"
   end
-  
+
 end
 
 ActionController::Base.send :include, AutoSessionTimeout
